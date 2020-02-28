f5268fd9ea0f18338cb7fb5f49cb8e3f8fb4350c
Amet <ametueraspirant@yahoo.com>
2020-02-28 05:54:18 -0700

    react app inittttttttttt

---
+++ src/components/Headbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './tailwind.css';
+
+class Headbar extends React.Component
+{
+	state =
+	{
+		vars: true
+	};
+
+	render()
+	{
+		return(
+			<div class = "flex flex-row">
+				stuff
+			</div>
+		);
+	}
+}
+
+export default Headbar;

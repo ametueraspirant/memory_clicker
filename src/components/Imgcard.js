e8846392996023d3f8c073028afc220599d2bd19
Amet <ametueraspirant@yahoo.com>
2020-03-04 04:54:45 -0700

    Updates

--- src/components/Imgcard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './tailwind.css';
-
-function Imgcard(props)
-{
-	return <img src={props.image} className = "w-1/12" id = {props.id} onClick = {props.onClick} alt = "tempalt" />;
-}
-
-export default Imgcard;
